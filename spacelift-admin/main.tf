ffecaac2cda84f93ef270cdb0ccae79e86c6eb20
Selubi <git@selubi.tech>
2025-07-12 00:42:35 +0900

    Add cloudflare token factory context

--- spacelift-admin/main.tf
+++ spacelift-admin/main.tf
@@ -27,3 +27,27 @@ resource "spacelift_stack" "spacelift_admin" {
   protect_from_deletion            = true
   labels                           = ["folder:component/platform"]
 }
+
+
+
+resource "spacelift_stack" "cloudflare_token_factory" {
+  space_id                         = "root"
+  name                             = "cloudflare-token-factory"
+  description                      = "Stack to create additional cloudflare tokens"
+  branch                           = "main"
+  repository                       = "platform"
+  project_root                     = "cloudflare-token-factory"
+  terraform_version                = var.opentofu_version
+  administrative                   = true # To register created tokens as spacelift contexts
+  autodeploy                       = true
+  terraform_smart_sanitization     = true
+  enable_well_known_secret_masking = true
+  protect_from_deletion            = true
+  labels                           = ["folder:component/platform"]
+}
+
+# cloudflare-token-factory-context is manually created
+resource "spacelift_context_attachment" "cloudflare_token_factor_context_attachment" {
+  context_id = "cloudflare-token-factory-context"
+  stack_id   = spacelift_stack.cloudflare_token_factory.id
+}
