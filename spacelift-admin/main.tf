f79cb47bbbed2e01fe6c733a8158d6676ad186c0
Selubi <git@selubi.tech>
2025-07-11 23:04:07 +0900

    Spacelift admin stack bootstrapping

---
+++ spacelift-admin/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    spacelift = {
+      source  = "spacelift-io/spacelift"
+      version = "1.25.0"
+    }
+  }
+}
+
+provider "spacelift" {}
+
+
+# This is the root stack that manages Spacelift itself.
+# It lives in the "root" space and creates other spaces and admin stacks.
+resource "spacelift_stack" "spacelift_admin" {
+  space_id                         = "root"
+  name                             = "spacelift-admin"
+  description                      = "Stack to manage Spacelift itself"
+  branch                           = "main"
+  repository                       = "platform"
+  project_root                     = "spacelift-admin"
+  autodeploy                       = true
+  administrative                   = true
+  terraform_smart_sanitization     = true
+  enable_well_known_secret_masking = true
+  protect_from_deletion            = true
+  labels                           = ["folder:component/platform"]
+}
