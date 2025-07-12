b8f808b027b8abe6c18aca5446e0f302fb24fd98
Selubi <git@selubi.tech>
2025-07-12 12:26:49 +0900

    Set the defaults for vars

--- cloudflare-admin/variables.tf
+++ cloudflare-admin/variables.tf
@@ -1,9 +1,11 @@
 variable "spacelift_stack_terraform_version" {
   description = "OpenTofu version to use"
   type        = string
+  default     = "1.1"
 }
 
 variable "spacelift_stack_terraform_workflow_tool" {
   description = "Specifies which flavor of tf"
   type        = string
+  default     = "OPEN_TOFU"
 }
