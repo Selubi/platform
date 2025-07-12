6df30ca6c027449b97f0c0f2cc06158a30d2559b
Selubi <git@selubi.tech>
2025-07-12 12:24:42 +0900

    Add a repo level config.tfvars

---
+++ config.tfvars
@@ -0,0 +1,2 @@
+spacelift_stack_terraform_version       = "1.1"
+spacelift_stack_terraform_workflow_tool = "OPEN_TOFU"
--- spacelift-admin/main.tf
+++ spacelift-admin/main.tf
@@ -10,6 +10,10 @@ terraform {
 provider "spacelift" {}
 
 
+######################################
+# Stacks
+######################################
+
 # This is the root stack that manages Spacelift itself.
 # It lives in the "root" space and creates other spaces and admin stacks.
 resource "spacelift_stack" "spacelift_admin" {
@@ -19,8 +23,8 @@ resource "spacelift_stack" "spacelift_admin" {
   branch                           = "main"
   repository                       = "platform"
   project_root                     = "spacelift-admin"
-  terraform_version                = var.opentofu_version
-  terraform_workflow_tool          = var.terraform_workflow_tool
+  terraform_version                = var.spacelift_stack_terraform_version
+  terraform_workflow_tool          = var.spacelift_stack_terraform_workflow_tool
   autodeploy                       = true
   administrative                   = true
   terraform_smart_sanitization     = true
@@ -37,14 +41,58 @@ resource "spacelift_stack" "cloudflare_admin" {
   branch                           = "main"
   repository                       = "platform"
   project_root                     = "cloudflare-admin"
-  terraform_version                = var.opentofu_version
-  terraform_workflow_tool          = var.terraform_workflow_tool
+  terraform_version                = var.spacelift_stack_terraform_version
+  terraform_workflow_tool          = var.spacelift_stack_terraform_workflow_tool
   autodeploy                       = true
   terraform_smart_sanitization     = true
   enable_well_known_secret_masking = true
   administrative = true  # To manage and grant tokens to other cloudflare stacks
   labels = ["folder:component/platform"]
 }
+
+#######################################
+# config.tfvars mounting
+#######################################
+
+resource "spacelift_context" "platform_config_vars" {
+  name        = "platform-config-vars"
+  description = "Configuration variables and settings for all platform stacks."
+  labels = ["folder:component/platform"] # Optional: labels for organization
+}
+
+resource "spacelift_mounted_file" "platform_config_tfvars_mounted" {
+  context_id = spacelift_context.platform_config_vars.id
+  # This path means the file will be at /mnt/workspace/config.tfvars
+  relative_path = "config.tfvars"
+  content = file("${path.module}/../config.tfvars") # This reads your file from your Git repo
+  write_only    = false
+}
+
+resource "spacelift_environment_variable" "platform_tf_var_files_env_var" {
+  context_id = spacelift_context.platform_config_vars.id
+  name       = "TF_VAR_FILES"
+  # This path instructs Terraform to go up from the project_root
+  # (e.g., from /mnt/workspace/source/my-repo/spacelift-admin/)
+  # to find the file at /mnt/workspace/source/my-repo/platform-common.tfvars
+  value      = "/mnt/workspace/config.tfvars"
+  write_only = false
+}
+
+
+resource "spacelift_context_attachment" "cloudflare_admin_config_attachment" {
+  context_id = spacelift_context.platform_config_vars.id
+  stack_id   = spacelift_stack.cloudflare_admin.id
+}
+
+resource "spacelift_context_attachment" "spacelift_admin_config_attachment" {
+  context_id = spacelift_context.platform_config_vars.id
+  stack_id   = spacelift_stack.spacelift_admin.id
+}
+
+
+#######################################
+# Manual contexts
+#######################################
 
 
 # cloudflare-admin-context is manually created
